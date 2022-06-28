66092b1bf163788578e72fa1f1e0d6a43544d9a8
paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-28 15:01:06 -0300

    feat: assets of MenuModal

---
+++ src/layout/MenuModal/assets/index.js
@@ -0,0 +1,12 @@
+import Astronaut from './astronaut.png';
+import Astronomy from './astronomy.png';
+import Friendship from './friendship.png';
+import Mission from './mission.png';
+import Medal from './medal.png';
+import Ranking from './ranking.png';
+import Training from './training.png';
+import Shop from './shop.png';
+
+export {
+  Astronaut, Astronomy, Friendship, Mission, Medal, Ranking, Training, Shop,
+};
