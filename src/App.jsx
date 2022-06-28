53896fba101f7207992e18cd351f288417c9d41d
paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-28 15:00:25 -0300

    feat: app.js MenuModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 
-import { ChakraProvider, Text } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './config/chakra-ui-config';
+// import CreateModule from './pages/CreateModule';
+import MenuModal from './layout/MenuModal';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Text color="solid_purple.100">Teste</Text>
+      {/* <Text color="solid_purple.100">Teste</Text> */}
+      {/* <CreateModule /> */}
+      <MenuModal />
     </ChakraProvider>
   );
 }
