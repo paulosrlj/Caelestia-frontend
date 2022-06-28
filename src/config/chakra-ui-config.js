6cfce0d9418dbd5b1d4c98d91d0d4a29cc06dcc3
paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-28 14:59:54 -0300

    chore: add chakra ui config

--- src/config/chakra-ui-config.js
+++ src/config/chakra-ui-config.js
@@ -1,17 +1,29 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const theme = extendTheme({
-  colors: {
-    solid_purple: {
-      100: '#A044FF',
+const breakpoints = {
+  sm: '320px',
+  '2sm': '550px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+};
+
+const theme = extendTheme(
+  {
+    colors: {
+      solid_purple: {
+        100: '#A044FF',
+      },
+      pure_green: {
+        100: '#00CD96',
+      },
+      main_red: {
+        100: '#FF4F19',
+      },
     },
-    pure_green: {
-      100: '#00CD96',
-    },
-    main_red: {
-      100: '#FF4F19',
-    },
+    breakpoints,
   },
-});
+);
 
 export { theme };
