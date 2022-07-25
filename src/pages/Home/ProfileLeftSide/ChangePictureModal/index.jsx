5abeca1db44e251664176c7b6962d39bed8402dd
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-25 18:03:52 -0300

    feat: add toastify to profile image upload

--- src/pages/Home/ProfileLeftSide/ChangePictureModal/index.jsx
+++ src/pages/Home/ProfileLeftSide/ChangePictureModal/index.jsx
@@ -5,20 +5,42 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import ImageUploading from 'react-images-uploading';
+
+// React toast
+import { toast } from 'react-toastify';
+import CustomToastContainer from '../../../../components/Toast/CustomToastContainer';
+import 'react-toastify/dist/ReactToastify.css';
+
 import uploadImageCallback from '../../../../util/uploadImageCallback';
 
 function ChangePictureModal({ isOpen, onClose, imageSrc }) {
   const [image, setImage] = useState([]);
   const [imageSrcUpload, setimageSrcUpload] = useState(null);
 
-  const onChange = async (imageList, addUpdateIndex) => {
-    console.log(imageList, addUpdateIndex);
+  const onChange = async (imageList) => {
     setImage(imageList);
+    if (imageList.length > 0) {
+      setimageSrcUpload(imageList[0].data_url);
+    }
+  };
+
+  const removeImage = () => {
+    setimageSrcUpload(null);
   };
 
   const uploadImage = async () => {
-    const { data: { link } } = await uploadImageCallback(image[0].file, 'profileImage');
-    setimageSrcUpload(link);
+    const { data: { link } } = await toast.promise(
+      uploadImageCallback(image[0].file, 'profileImage'),
+      {
+        pending: 'Fazendo upload...',
+        success: 'Foto de perfil atualizada com sucesso!',
+        error: 'Ocorreu um problema.',
+      },
+    );
+
+    if (link !== undefined) {
+      setimageSrcUpload(link);
+    }
   };
 
   return (
@@ -27,9 +49,23 @@ function ChangePictureModal({ isOpen, onClose, imageSrc }) {
       <ModalContent>
         <ModalHeader color="solid_purple.100" textAlign="center">Mudar foto de perfil</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
+        <ModalBody display="flex" justifyContent="center">
 
-          <Image src={imageSrcUpload || imageSrc} size={75} />
+          <Image
+            minW={[
+              '60%',
+            ]}
+            w={[
+              '60%',
+            ]}
+            h={[
+              '150px',
+              '250px',
+              '250px',
+            ]}
+            borderRadius="50%"
+            src={imageSrcUpload || imageSrc}
+          />
 
         </ModalBody>
 
@@ -44,7 +80,6 @@ function ChangePictureModal({ isOpen, onClose, imageSrc }) {
             onImageRemove,
             dragProps,
           }) => (
-            // write your building UI
             <ModalFooter flex justifyContent="space-evenly">
 
               <Button
@@ -58,9 +93,15 @@ function ChangePictureModal({ isOpen, onClose, imageSrc }) {
               <Button color="whiteAlpha.900" bgColor="pure_green.100" onClick={uploadImage}>
                 Salvar
               </Button>
-              <Button onClick={() => onImageRemove(0)} color="whiteAlpha.900" bgColor="main_red.100">
+              <Button
+                onClick={() => [onImageRemove(0), removeImage()]}
+                color="whiteAlpha.900"
+                bgColor="main_red.100"
+              >
                 Remover
               </Button>
+
+              <CustomToastContainer />
             </ModalFooter>
           )}
         </ImageUploading>
